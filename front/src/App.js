1d9e4dbcf25ac910e8d2503456d998a67e8b0f19
Laure-Amélie Colin <laureamelie.colin@telecom-paris.fr>
2021-06-23 14:10:06 +0200

    initialisation du projet

---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./App.css"; 
+
+function App() {
+  return (
+    <div  className='App-header'> 
+      <h1> Associations des Télécommiens :  </h1>
+      <h3> Vous avez des difficultés à bien choisir l'association qui vous convient le plus ? Répondez à ces questions :</h3>
+      <h3> Question 1 :  </h3> <button> Reponse 1  </button> <button> Reponse 2  </button> <button> Reponse 3  </button> <button> Reponse 3  </button>
+      <h3> Question 2 : </h3> <button> Reponse 1  </button> <button> Reponse 2  </button> <button> Reponse 3  </button> <button> Reponse 3  </button>
+      <h3> Question 3 :  </h3> <button> Reponse 1  </button> <button> Reponse 2  </button> <button> Reponse 3  </button> <button> Reponse 3  </button>
+      <h3> Question 4 : </h3> <button> Reponse 1  </button> <button> Reponse 2  </button> <button> Reponse 3  </button> <button> Reponse 3  </button>
+    </div>
+  );
+}
+
+export default App;
