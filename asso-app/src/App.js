2fa102be31409b7599b0bcfc8d98618ea07b1cd3
Abdelghani Harrad <abdelghani.harrad@telecom-paris.fr>
2021-06-24 10:25:53 +0200

    restauration du front

--- asso-app/src/App.js
+++ asso-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div  className='App-header'> 
       <h1> Associations des Télécommiens :  </h1>
       <h3> Vous avez des difficultés à bien choisir l'association qui vous convient le plus ? Répondez à ces questions :</h3>
-      <h3> Question 1 :  </h3> <button> Reponse 1  </button> <button> Reponse 2  </button> <button> Reponse 3  </button> <button> Reponse 3  </button>
+      <h3 className="App-link" > Question 1 :  </h3> <button> Reponse 1  </button> <button> Reponse 2  </button> <button> Reponse 3  </button> <button> Reponse 3  </button>
       <h3> Question 2 : </h3> <button> Reponse 1  </button> <button> Reponse 2  </button> <button> Reponse 3  </button> <button> Reponse 3  </button>
       <h3> Question 3 :  </h3> <button> Reponse 1  </button> <button> Reponse 2  </button> <button> Reponse 3  </button> <button> Reponse 3  </button>
       <h3> Question 4 : </h3> <button> Reponse 1  </button> <button> Reponse 2  </button> <button> Reponse 3  </button> <button> Reponse 3  </button>
